d8901ae341ad128fe44d18659b69f0aacef79d85
Tatsu <Tatsu@9078533b-c07c-b04a-bfb7-44a446c97fdc>
2013-08-11 05:31:18 +0000

    verDialog.hの登録が漏れていたので追加した。
    git-svn-id: file:///H/repositry/imex/trunk@6 9078533b-c07c-b04a-bfb7-44a446c97fdc

---
+++ savlist/verDialog.h
@@ -0,0 +1,11 @@
+/*
+SAVList (C) 2005 Tatsuhiko Shoji
+The sources for SAVList are distributed under the MIT open source license
+*/
+#ifndef VERDIALOG_H
+#define VERDIALOG_H
+
+BOOL CALLBACK VerDialogProc(HWND hDlg,UINT msg,WPARAM wp,LPARAM lp);
+
+#endif
+
